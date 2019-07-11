2c3e24332acda726323d38bc202a5e5dba33b2b6
wujiyang <1441615466@qq.com>
2019-07-11 21:52:48 +0800

    PPS

--- SimpleH264Analyzer/PicParamSet.h
+++ SimpleH264Analyzer/PicParamSet.h
@@ -25,21 +25,21 @@ public:
 	void Dump_pps_info();
 
 private:
-	UINT8  pps_id;
-	UINT8  sps_id;
-	bool   entropy_coding_flag;
-	bool   bottom_field_pic_order_in_frame_present_flag;
-	UINT8  num_slice_groups;
-	UINT8  num_ref_idx_l0_default_active;
-	UINT8  num_ref_idx_l1_default_active;
-	bool   weighted_pred_flag;
-	UINT8  weighted_bipred_idc;
-	int    pic_init_qp;
-	int    pic_init_qs;
-	int    chroma_qp_index_offset;
-	bool   deblocking_filter_control_present_flag;
-	bool   constrained_intra_pred_flag;
-	bool   redundant_pic_cnt_present_flag;
-	bool   transform_8x8_mode_flag;
+	UINT8  m_pps_id;
+	UINT8  m_sps_id;
+	bool   m_entropy_coding_flag;
+	bool   m_bottom_field_pic_order_in_frame_present_flag;
+	UINT8  m_num_slice_groups;
+	UINT8  m_num_ref_idx_l0_default_active;
+	UINT8  m_num_ref_idx_l1_default_active;
+	bool   m_weighted_pred_flag;
+	UINT8  m_weighted_bipred_idc;
+	int    m_pic_init_qp;
+	int    m_pic_init_qs;
+	int    m_chroma_qp_index_offset;
+	bool   m_deblocking_filter_control_present_flag;
+	bool   m_constrained_intra_pred_flag;
+	bool   m_redundant_pic_cnt_present_flag;
+	bool   m_transform_8x8_mode_flag;
 };
 
